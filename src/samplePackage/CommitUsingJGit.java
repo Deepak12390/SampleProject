30abc166eb1895b575ed7da0b8bc1a2788386600
Akash Patel <Akash.Patel@SYNCSS.LOCAL>
2020-03-03 08:52:57 -0600

    Brach commit

--- src/samplePackage/CommitUsingJGit.java
+++ src/samplePackage/CommitUsingJGit.java
@@ -20,7 +20,7 @@ public class CommitUsingJGit {
 
 	public static void main(String[] args) throws IOException, GitAPIException {
 
-		Repository existingRepo = new FileRepositoryBuilder().setGitDir(new File("C:/eclipse/Workspace/SampleDemo")).build();
+		Repository existingRepo = new FileRepositoryBuilder().setGitDir(new File("C:/eclipse/Workspace/SampleDemo/.git")).build();
 
 		// Monitor to get git command progress printed on java System.out
 		// console
@@ -36,27 +36,23 @@ public class CommitUsingJGit {
 		System.out.print("\n>>> Modified file\n");
 		status.getModified().stream().filter(str -> str.endsWith(".java")).forEach(System.out::println);
 
-		System.out.print("\n>>> Untracked/Newly Created file\n");
-		status.getUntracked().stream().filter(str -> str.endsWith(".java")).forEach(System.out::println);
-
 		System.out.print("\n>>> Added file\n");
 		status.getAdded().stream().filter(str -> str.endsWith(".java")).forEach(System.out::println);
 
 		// Commit a particular file in the project
 		System.out.println("\n>>> Committing changes\n");
-		RevCommit revCommit = git.commit().setOnly("src/test/java/HelloWorld.java").setMessage("Adding commit from JGIT")
+		RevCommit revCommit = git.commit().setOnly("src/samplePackage/HelloWorld.java").setMessage("Adding commit from JGIT")
 				.call();
 
 		System.out.println("Commit = " + revCommit.getFullMessage());
 
 		// Providing credentials to push code to remote repository
-		/*CredentialsProvider cp = new UsernamePasswordCredentialsProvider("Deepak12390", "Rehan@31");
+		CredentialsProvider cp = new UsernamePasswordCredentialsProvider("Deepak12390", "Rehan@31");
 		Iterable<PushResult> pushLog = git.push().setCredentialsProvider(cp).setPushAll().call();
-		pushLog.forEach(r -> System.out.println(r.getMessages()));*/
+		pushLog.forEach(r -> System.out.println(r.getMessages()));
 
 		// Close Git
 		git.close();
 
 	}
-
 }
--- src/samplePackage/HelloWorld.java
+++ src/samplePackage/HelloWorld.java
@@ -4,7 +4,6 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-          System.out.println("Hello World I");
+          System.out.println("Hello World I2");
 	}
-
 }
